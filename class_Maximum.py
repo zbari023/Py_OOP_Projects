2903c1ba930ccbb8afd4d5402864777fd479c38a
zbari023 <barry450643@gmail.com>
2023-03-12 21:35:46 +0100

    new apdate

--- class_Maximum.py
+++ class_Maximum.py
@@ -1,4 +1,7 @@
 
 # my fist class that give you which number is bigger
 class Maximum:
+    print("Max of 2 values: ")
+    x = int(input("enter your first number: "))
+    y = int(input("enter your secund number: "))
 
