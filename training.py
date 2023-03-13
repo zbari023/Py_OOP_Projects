b25d7b40b8aa5e328787c9e87150f87b5528f5a5
zbari023 <barry450643@gmail.com>
2023-03-13 17:38:18 +0100

    new work

--- training.py
+++ training.py
@@ -1,11 +1,17 @@
 class Car:  # class
-    # property
+    # property or Attribute
     color = 'red'
     modell = 2018
     motor = 'Benzin'
     # Methods or functions
-    def kilometerage(self,amount):
+    def kilometerage(self,amount,limit):   # encapculation that placedholder for the object
         self.amount = amount
+        self.limit = limit
+        print(f"The Car drived {self.amount} and can go untel {self.limit}")
+
+    def __init__(self):       # constructor, a special method that automatically called at the begin
+        print("Welcome in this Automarkt :")
+
 
 
 print("Object 1 of class Car :")
@@ -16,3 +22,7 @@ c1.motor = 'Desil'
 print(c1.motor)
 c1.modell = 'Maybach 2023'
 print(c1.modell)
+
+print("the output of function: ")
+c1.kilometerage("127 km","200 km")
+
